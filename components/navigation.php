aa481f816df6ee8a49a719a4595602e037ea28d7
boltkaster <boltkasterr@gmail.com>
2025-03-25 19:02:45 +0100

    separationcom
    seperated header, navigation and footer into different files.

---
+++ components/navigation.php
@@ -0,0 +1,12 @@
+<nav class="main-nav">
+    <ul class="main-menu" id="main-menu">
+        <li><a href="index.php">Domov</a></li>
+        <li><a href="portfolio.php">Portfólio</a></li>
+        <li><a href="qna.php">Q&A</a></li>
+        <li><a href="kontakt.php">Kontakt</a></li>
+    </ul>
+    <a class="hamburger" id="hamburger">
+        <i class="fa fa-bars"></i>
+    </a>
+</nav>
+</header>
